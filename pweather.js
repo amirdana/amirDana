fcf34e0bbcbc143347037583c829859f9bc9f7e5
amirdana <lavasaniamirhossein@gmail.com>
2019-08-28 20:16:04 +0430

    'bug-fixed'

--- pweather.js
+++ pweather.js
@@ -1,36 +1,27 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
-      console.log(long);
-      console.log(lat);
 
-      const api = `https://api.darksky.net/forecast/645471e083e551402049938d9fbf4b8e/${lat},${long}`;
-      fetch(api)
-        .then(answer => {
-          return answer.json();
-        })
-        .then(dataz => {
-          document.getElementById("preloader").style.display = "none";
-          console.log(dataz);
-          zone = dataz.timezone;
-          document.querySelector(".t-z").innerHTML = zone;
-          document.querySelector(".w-h").innerHTML =
-            Math.round(dataz.currently.humidity * 100) + " %";
-          document.querySelector(".w-s").innerHTML =
-            dataz.currently.windSpeed + " km";
-          document.querySelector(".w-temp").innerHTML =
-            Math.round(((dataz.currently.temperature - 32) * 5) / 9) + " °C";
+  lat = 35.7248;
+  long = 51.3817;
 
-          setIcons(dataz.currently.icon, document.querySelector(".icon"));
-        });
+  const api = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/645471e083e551402049938d9fbf4b8e/${lat},${long}`;
+  fetch(api)
+    .then(answer => {
+      return answer.json();
+    })
+    .then(data => {
+      document.getElementById("preloader").style.display = "none";
+      zone = data.timezone;
+      document.querySelector(".t-z").innerHTML = zone;
+      document.querySelector(".w-h").innerHTML =
+        Math.round(data.currently.humidity * 100) + " %";
+      document.querySelector(".w-s").innerHTML =
+        data.currently.windSpeed + " km";
+      document.querySelector(".w-temp").innerHTML =
+        Math.round(((data.currently.temperature - 32) * 5) / 9) + " °C";
+      setIcons(data.currently.icon, document.querySelector(".icon"));
     });
-  } else {
-    alert("This function need locaion permision");
-  }
 
   function setIcons(icon, iconId) {
     const skycons = new Skycons({ color: "white" });
